c50a126ae94a641cf928a7c4aaacb79be950fd82
Philipp Schiweck <philipp.schiweck@ooglemail.com>
2021-05-07 16:17:03 +0200

    Change Piecetype Enum Values are added to PieceType; Added constructor and and get method.

--- src/ChessClient/Pieces/PieceType.java
+++ src/ChessClient/Pieces/PieceType.java
@@ -1,10 +1,20 @@
 package ChessClient.Pieces;
 
 public enum PieceType {
-    QUEEN,
-    KING,
-    ROOK,
-    BISHOP,
-    KNIGHT,
-    PAWN
+    QUEEN(9),
+    KING(10000),
+    ROOK(5),
+    BISHOP(3),
+    KNIGHT(3),
+    PAWN(1);
+
+    private final int value;
+
+    PieceType(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
