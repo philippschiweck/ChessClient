7f8333fc64527446b14bfc5c12a8da2136b7dee0
Philipp Schiweck <philipp.schiweck@ooglemail.com>
2021-05-09 17:17:02 +0200

    Add initial Board setup function

--- src/ChessClient/Chesslogic/Board/BoardSetup.java
+++ src/ChessClient/Chesslogic/Board/BoardSetup.java
@@ -1,6 +1,7 @@
 package ChessClient.Chesslogic.Board;
 
-import ChessClient.Chesslogic.Board.Field;
+import ChessClient.Pieces.ChessColor;
+import ChessClient.Pieces.PieceTypes.*;
 
 public final class BoardSetup {
 
@@ -17,6 +18,35 @@ public final class BoardSetup {
                 initialSetup[i][j] = new Field(null, (char)(column + i), j);
             }
         }
+
+        //White Backrank
+        initialSetup[0][0].setPiece(new Rook(ChessColor.WHITE));
+        initialSetup[0][1].setPiece(new Knight(ChessColor.WHITE));
+        initialSetup[0][2].setPiece(new Bishop(ChessColor.WHITE));
+        initialSetup[0][3].setPiece(new Queen(ChessColor.WHITE));
+        initialSetup[0][4].setPiece(new King(ChessColor.WHITE));
+        initialSetup[0][5].setPiece(new Bishop(ChessColor.WHITE));
+        initialSetup[0][6].setPiece(new Knight(ChessColor.WHITE));
+        initialSetup[0][7].setPiece(new Rook(ChessColor.WHITE));
+        //White Pawns
+        for(int i = 0; i < initialSetup[1].length; i++){
+            initialSetup[1][i].setPiece(new Pawn(ChessColor.WHITE));
+        }
+
+        //Black Pawns
+        for(int i = 0; i < initialSetup[1].length; i++){
+            initialSetup[6][i].setPiece(new Pawn(ChessColor.BLACK));
+        }
+
+        //Black Backrank
+        initialSetup[7][0].setPiece(new Rook(ChessColor.BLACK));
+        initialSetup[7][1].setPiece(new Knight(ChessColor.BLACK));
+        initialSetup[7][2].setPiece(new Bishop(ChessColor.BLACK));
+        initialSetup[7][3].setPiece(new Queen(ChessColor.BLACK));
+        initialSetup[7][4].setPiece(new King(ChessColor.BLACK));
+        initialSetup[7][5].setPiece(new Bishop(ChessColor.BLACK));
+        initialSetup[7][6].setPiece(new Knight(ChessColor.BLACK));
+        initialSetup[7][7].setPiece(new Rook(ChessColor.BLACK));
 
         return initialSetup;
     }
