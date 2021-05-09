c41a4cc430ee771a4356dafb99a8fafeab22fc5f
Philipp Schiweck <philipp.schiweck@ooglemail.com>
2021-05-09 17:56:04 +0200

    Add init and addPieces to BoardGui

--- src/ChessClient/GUI/BoardGui/BoardGui.java
+++ src/ChessClient/GUI/BoardGui/BoardGui.java
@@ -1,9 +1,26 @@
 package ChessClient.GUI.BoardGui;
 
+import ChessClient.Chesslogic.Board.Board;
+
 import javax.swing.*;
 
-public class BoardGui extends JPanel {
+public class BoardGui extends JLayeredPane {
 
+    private Board board;
 
+    public BoardGui(Board board){
+        this.board = board;
+
+        initBoardGui();
+        addPieces();
+    }
+
+    private void initBoardGui(){
+
+    }
+
+    private void addPieces(){
+
+    }
 
 }
