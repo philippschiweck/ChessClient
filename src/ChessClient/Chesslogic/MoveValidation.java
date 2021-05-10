e4dd0f23ec1ab66c790bc2defc99028ca8b28c3c
Philipp Schiweck <philipp.schiweck@googlemail.com>
2021-05-10 14:24:06 +0200

    Remove move validation from MoveValidation class and add it to individual piece classes

--- src/ChessClient/Chesslogic/MoveValidation.java
+++ src/ChessClient/Chesslogic/MoveValidation.java
@@ -5,57 +5,19 @@ import ChessClient.Chesslogic.Board.FieldPosition;
 
 public final class MoveValidation {
 
-    public static boolean validateRook(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
-    public static boolean validatePawn(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
-
-    public static boolean validateKing(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
-
-    public static boolean validateQueen(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
-
-    public static boolean validateKnight(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
-
-    public static boolean validateBishop(BoardState currentBoardState, FieldPosition currentFieldPosition, FieldPosition targetFieldPosition){
-        boolean isMoveValid = false;
-
-
-        return isMoveValid;
-    }
-
     public static boolean checkForCheckmate(){
         boolean checkMate = false;
 
 
         return checkMate;
+    }
+
+    public static boolean isMoveInbounds(int targetCol, int targetRow){
+        return (targetCol >= 0 && targetCol <= 7) && (targetRow >= 0 && targetRow <= 7);
+    }
+
+    public static boolean isNotSameField(int currentCol, int currentRow, int targetCol, int targetRow){
+        return (currentCol != targetCol && currentRow != targetRow);
     }
 
 
